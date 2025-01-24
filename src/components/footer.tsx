ad555c428dddf7b48e5c0fdb3f3a92c35ad7a09f
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2025-01-25 02:27:27 +0530

    styling fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,9 @@
-import { Github } from "lucide-react"
 import Link from "next/link"
 
 export function Footer() {
     return (
-        <footer className="w-full border-t bg-background">
-            <div className="container max-w-7xl mx-auto flex flex-col items-center justify-between gap-4 py-12 md:h-32 md:flex-row md:py-0">
+        <footer className="w-full border-t bg-background items-center mx-auto">
+            <div className="container max-w-7xl mx-auto flex flex-col items-center justify-center gap-4 py-12 md:h-32 md:flex-row md:py-0">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
                         Built by{" "}
